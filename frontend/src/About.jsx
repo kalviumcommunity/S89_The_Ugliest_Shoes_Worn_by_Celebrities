a8b6da695ee2415b83e77efce7f6a40c193717ca
chethan-gen <chaithuregala123@gmailcom>
2025-03-29 10:55:59 +0530

    adding files

---
+++ frontend/src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About:</h1>
+        <h1>Tittle: The Ugliest Shoes Worn By Celebrities</h1>
+        <h2>This website displays all the moments when the Celebrities wore the ugliest shoes in public.</h2>
+    </div>
+    
+  )
+}
+
+export default About
